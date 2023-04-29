48d819fc8284844c11c12866a4514f0bfa8514a6
Eric Kayiranga <tipulive@gmail.com>
2023-04-29 19:06:24 +0200

    create UserController and add Authentication and middleware

---
+++ Backend/Controllers/UserController.js
@@ -0,0 +1,161 @@
+const db_connect=require('../Config/db_config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { expressjwt } = require('express-jwt');
+const connection =db_connect.database_config;
+
+
+
+exports.UserController = {
+
+  registerUser:async(req)=>{
+   
+    const { name,email, password } = req.body;
+
+    return new Promise(function(resolve, reject) {
+      var returnValue = "";
+      try {
+        const query = 'SELECT * FROM users WHERE email = ?';
+        connection.query(query, [email], async (error, results) => {
+          if (error) {
+         
+
+            returnValue = {
+                  
+              status:false,
+              message:"Server error"
+              };
+              resolve(returnValue);
+          }
+    
+          if (results.length > 0) {
+            
+            returnValue = {
+                  
+              status:false,
+              message:"Email already exists"
+              };
+              resolve(returnValue);
+          }
+          else{
+            
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const insertQuery = 'INSERT INTO users (name,email, password) VALUES (?,?,?)';
+            connection.query(insertQuery, [name,email,hashedPassword], (error, results) => {
+              if (error) {
+                
+                returnValue= {
+                    
+                  status:false,
+                  message:"Server error"
+                  };
+              }
+              if(results.affectedRows> 0)
+              {
+                returnValue = {
+                    
+                  status:true,
+                  message:"User created"
+                  };
+              }
+             
+      
+              resolve(returnValue);
+              
+                
+            });
+          }
+    
+        
+        
+        });
+      } catch (error) {
+        
+        returnValue = {
+                  
+          status:true,
+          message:"Server error"
+          };
+          resolve(returnValue);
+      }
+     
+  });
+  
+    
+  },
+ 
+  login:async(req)=>{
+
+
+    const { email, password } = req.body;
+
+    // TODO: Validate the user's credentials
+    
+
+    return new Promise(function(resolve, reject) {
+      var returnValue = "";
+  
+    /*const token = jwt.sign({ username }, 'testsecret');
+  
+    res.json({ token });*/
+
+
+    
+  try {
+    const query = 'SELECT * FROM users WHERE email = ?';
+    connection.query(query, [email], async (error, results) => {
+      if (error) {
+       
+        returnValue = {
+                  
+          status:false,
+          message:"Server error"
+          };
+
+      }
+
+      if (results.length === 0) {
+      
+        returnValue = {
+                  
+          status:false,
+          message:"Invalid email or password"
+          };
+      }
+
+      const user = results[0];
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) {
+        returnValue = {
+                  
+          status:false,
+          message:"Invalid email or password"
+          };
+      }
+
+      const token = jwt.sign({ email },process.env.JWT_KEY, { expiresIn: '1h' });
+      
+     // res.status(200).json({ token });
+
+      returnValue = {
+                  
+        status:true,
+        token:token
+        };
+     
+        resolve(returnValue);
+    });
+  } catch (error) {
+    
+    returnValue = {
+                  
+      status:false,
+      message:"Server error"
+      };
+      resolve(returnValue);
+  }
+
+});
+
+  }
+}
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,4 +1,6 @@
 const express=require('express');
+const User=require("../Controllers/UserController");
+const jwt = require('jsonwebtoken');
 const router=express.Router();
 
 
@@ -6,16 +8,43 @@ const router=express.Router();
 
 router.post('/register',async(req,res) => {
     
-    
+    let resultData=await User.UserController.registerUser(req);
+    res.json(resultData);
 
 });
 
 router.post('/login',async(req,res) => {
     
-    
+    let resultData=await User.UserController.login(req);
+    res.json(resultData);
 
 });
 
+router.get('/protect', CheckAuthToken, (req, res) => {
+    res.json({ message: `Hello, ${req.user.email}!` });
+  });
+function CheckAuthToken(req, res, next) {
+    // Get the authorization header
+    const authHeader = req.headers['authorization'];
+    // Extract the JWT token from the authorization header
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) {
+      // Return an error response if the JWT token is missing
+      return res.sendStatus(401);
+    }
+    // Verify the JWT token using the secret key
+    jwt.verify(token,process.env.JWT_KEY, (err, user) => {
+      if (err) {
+        // Return an error response if the JWT token is invalid
+        return res.sendStatus(403);
+        //res.status(403).json({ message: 'Authentication is invalid error' });
+      }
+      // Store the user object in the request object
+      req.user = user;
+      // Call the next middleware function
+      next();
+    });
+  }
 
 
 module.exports=router;
